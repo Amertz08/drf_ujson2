029aed48e1eb35e149b0cfbc3de7243c0845fac7
Adam Mertz <adammertz@gmail.com>
2019-11-05 21:59:33 -0600

    bump version 1.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="drf_ujson2",
-    version="1.4",
+    version="1.4.1",
     python_requires=">=3.4",
     description="Django Rest Framework UJSON Renderer",
     long_description=long_description,
